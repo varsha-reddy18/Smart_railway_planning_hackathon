a06f0fb123a5d78b5e24e77a5378c903dfdc7bc7
varsha-reddy18 <23b81a7255@cvr.ac.in>
2026-03-04 10:55:09 +0530

    updated

--- app/app.py
+++ app/app.py
@@ -3,16 +3,10 @@ import pandas as pd
 import joblib
 import os
 
-def load_css():
-    css_path = os.path.join(os.path.dirname(__file__), "style.css")
-    with open(css_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-load_css()
 
 st.title('🚆 Smart Railway Resource Planning Dashboard')
 
-df = pd.read_csv("data/railway_data.csv")
+df = pd.read_csv("../data/railway_data.csv")
 
 st.subheader("Dataset Preview")
 st.dataframe(df.head())
@@ -20,7 +14,7 @@ st.dataframe(df.head())
 st.subheader("Passenger Distribution")
 st.bar_chart(df["passenger_count"])
 
-model = joblib.load("models/delay_model.pkl")
+model = joblib.load("../models/delay_model.pkl")
 
 st.sidebar.header("Train Inputs")
 
@@ -30,9 +24,35 @@ passengers = st.sidebar.number_input("Passenger Count",100,2000)
 platform = st.sidebar.slider("Platform Number",1,6)
 track = st.sidebar.slider("Track Number",1,4)
 
+# Check platform conflict
+conflict = df[
+    (df["platform_number"] == platform) &
+    (df["arrival_hour"] == arrival)
+]
+
+if len(conflict) > 0:
+    st.warning("⚠ Platform Conflict Detected!")
+
+    free_platforms = list(set(range(1,7)) - set(df["platform_number"]))
+
+    if len(free_platforms) > 0:
+        st.info(f"Suggested Free Platform: {free_platforms[0]}")
+    else:
+        st.error("No platforms available at this time")
+
 if st.sidebar.button("Predict Delay"):
 
+    # Conflict detection
+    conflict = df[
+        (df["platform_number"] == platform) &
+        (df["arrival_hour"] == arrival)
+    ]
+
+    if len(conflict) > 0:
+        st.warning("⚠ Platform Conflict Detected!")
+
     data = [[arrival,departure,passengers,platform,track]]
+
     prediction = model.predict(data)
 
     st.success(f"Predicted Delay: {prediction[0]:.2f} minutes")
