539cac23a4d451c28c485bd9d57be6ed03a1259c
varsha-reddy18 <23b81a7255@cvr.ac.in>
2026-03-04 10:38:44 +0530

    updated

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import joblib
 import os
 
 def load_css():
-    css_path = os.path.join(os.path.dirname(__file__), "style.css")
+    css_path = os.path.join(os.path.dirname(__file__), "app/style.css")
     with open(css_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 load_css()
